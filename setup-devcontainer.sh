335fb43bbaa90f77181df4ba11c212d85281a19a
James Thurley <james@thurley.com>
2024-02-20 13:38:58 +0000

    Configure starship

--- setup-devcontainer.sh
+++ setup-devcontainer.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+sudo apt update
 sudo apt install autojump -y
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraCode.zip
+unzip FiraCode.zip -d ~/.fonts
+fc-cache -fv
+
+curl -sS https://starship.rs/install.sh | sh -s -- -y
 
 ./install-zsh.sh
 ./link-dotfiles.sh
