9efa459169d29f03225706e2bb3eef456546b7b1
James Thurley <james@thurley.com>
2024-02-20 13:50:26 +0000

    Faster way to install nerd fonts

---
+++ install-nerd-fonts.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# https://gist.github.com/matthewjberger/7dd7e079f282f8138a9dc3b045ebefa0?permalink_comment_id=4563280#gistcomment-4563280
+
+declare -a fonts=(
+    # BitstreamVeraSansMono
+    # CodeNewRoman
+    # DroidSansMono
+    FiraCode
+    # FiraMono
+    # Go-Mono
+    # Hack
+    # Hermit
+    # JetBrainsMono
+    # Meslo
+    # Noto
+    # Overpass
+    # ProggyClean
+    # RobotoMono
+    # SourceCodePro
+    # SpaceMono
+    # Ubuntu
+    # UbuntuMono
+)
+
+fonts_dir="${HOME}/.local/share/fonts"
+
+if [[ ! -d "$fonts_dir" ]]; then
+    mkdir -p "$fonts_dir"
+fi
+
+echo -e "\e[0;32mScript:\e[0m \e[0;34mClonning\e[0m \e[0;31mNerdFonts\e[0m \e[0;34mrepo (sparse)\e[0m"
+git clone --filter=blob:none --sparse git@github.com:ryanoasis/nerd-fonts
+cd nerd-fonts
+
+for font in "${fonts[@]}"; do
+    echo -e "\e[0;32mScript:\e[0m \e[0;34mClonning font:\e[0m \e[0;31m${font}\e[0m"
+    git sparse-checkout add "patched-fonts/${font}"
+    echo -e "\e[0;32mScript:\e[0m \e[0;34mInstalling font:\e[0m \e[0;31m${font}\e[0m"
+    ./install.sh "${font}"
+done
+
+echo -e "\e[0;32mScript:\e[0m \e[0;34mCleaning the mess...\e[0m"
+cd ../
+rm -rf nerd-fonts```
--- setup-devcontainer.sh
+++ setup-devcontainer.sh
@@ -4,10 +4,7 @@
 sudo apt update
 sudo apt install autojump -y
 
-# Install FiraCode for Starship
-git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git    # warning: takes a while
-cd nerd-fonts/
-./install.sh FiraCode
+./install-nerd-fonts.sh
 
 # Install Starship
 curl -sS https://starship.rs/install.sh | sh -s -- -y
